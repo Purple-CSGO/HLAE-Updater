2f7772608747fad9edc02e6c232f32556ab63aac
purp1e <438518244@qq.com>
2020-08-04 19:48:08 +0800

    Set up build scripts for app.go and cli.go

---
+++ build.cmd
@@ -0,0 +1,15 @@
+@echo off
+chcp 65001
+
+:: 编译APP可执行文件
+go build -o %GOPATH%/bin/HLAE-Updater.exe app.go
+:: 编译CLI命令行可执行文件
+cd CLI
+go build -o %GOPATH%/bin/HLAE-Updater-CLI.exe cli.go
+echo ----------------------- APP -----------------------
+:: 运行APP可执行文件
+HLAE-Updater.exe
+echo ----------------------- CLI -----------------------
+:: 运行CLI命令行可执行文件
+HLAE-Updater-CLI.exe
+echo ----------------------- END -----------------------
